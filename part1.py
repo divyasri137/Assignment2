2569800f7337fcacdf59c7ba97d0ed063fa49891
divyasri137 <70433576+divyasri137@users.noreply.github.com>
2021-07-10 13:35:19 +0530

    Add files via upload

--- part1.py
+++ part1.py
@@ -1,6 +1,11 @@
-sentense=input("Enter your sentense \n")
+from collections import defaultdict
 
-replace1 = sentense.replace("h","d").replace("o","zm")
-    
-print(replace1.capitalize())
-print(replace1.lower())
+od = defaultdict()
+od['a'] = input("input1\n")
+od['b'] = input("input2\n")
+od['c'] = input("input3\n")
+od['d'] = input("input4\n")
+od['e'] = input("input5\n")
+
+num = [od]
+print(num)
--- part2.py
+++ part2.py
@@ -1,6 +1,14 @@
-string=input("Enter a string (length should be at least 3)\n")
+from collections import OrderedDict
+  
+od = OrderedDict()
+od['a'] = input("input1\n")
+od['b'] = input("input2\n")
+od['c'] = input("input3\n")
+od['d'] = input("input4\n")
+od['e'] = input("input5\n")
+ 
 
-print(string+"ing")
-
-if string.endswith('ing'):
-    print(string.replace("ing","ly"))
+if od.values() % 2==0:
+    print(od.values() + "0")
+else:
+    print(od.values()+"1")
